83b797738492d15937501bc660ad1207ed121ab7
mwood77 <43637076+mwood77@users.noreply.github.com>
2022-03-23 21:26:07 +0100

    refactored and simplied

--- beocom-bell/beocom-bell.ino
+++ beocom-bell/beocom-bell.ino
@@ -1,17 +1,14 @@
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-unsigned long startTime;
-unsigned long endMicros;
 
 const int LED = LED_BUILTIN;
-const int TEST_RINGER_BUTTON = 4;   // D4
-const int RELAY_PIN = 7;            // D7
-const int RF_PIN = 2;               // Pin D2 = interrupt 0
-const int HZ = 25;
-
-int ledState = LOW;                 // Initial state used to set the LED off
-int buttonState = 0;                // Used to read current button state
+const int TEST_RINGER_BUTTON = 4;        // D4
+const int RELAY_PIN = 7;                 // D7
+const int RF_PIN = 0;                    // Pin D2 = interrupt 0
+int ledState = LOW;                      // Initial state used to set the LED off
+int buttonState = 0;                     // Used to read current button state
+const int RING_DURATION = 2000;          // How long you want your ringer to be triggered
 
 void setup() {
 	Serial.begin(9600);
@@ -39,33 +36,17 @@ void busyLED(byte state) {
 	digitalWrite(LED, state); 
 }
 
-/*
-  Ring voltage approv 50V @ 25HZ for 2 seconds.
-  1000 miliseconds / 25(hz) = 20ms high and 20ms low @ 50% duty cycle
-
-  HARDCODED FOR 25HZ OVER 2 SECONDS
-*/
 void generateRingtone() {
-	int count = 0;
-	busyLED(HIGH);
+  Serial.println(F("Ringing"));
 
-	Serial.println("Ringing");
-	startTime = millis();
-	for (int i = 0; i < HZ * 2; i++ ) {
-		digitalWrite(RELAY_PIN, LOW);
-		delay(20);			// Doorbell is ringing
-		digitalWrite(7, HIGH);
-		delay(20);			// Doorbell is ringing
-	count++;
-	}
-	endMicros = millis();
+	busyLED(HIGH);
+  digitalWrite(RELAY_PIN, LOW); 
+  delay(RING_DURATION);		          	   // Doorbell is ringing  	
+  digitalWrite(7, HIGH);
 	busyLED(LOW);
 
-	Serial.print("Ring duration: ");
-	Serial.print( (endMicros - startTime) / 1000 );
-	Serial.print(" seconds    |    Times cycled: ");
-	Serial.println(count);
+	Serial.println(F("Hang-up"));
 
 	// Cool down
-	delay(DURATION * 2);
+	delay(4000);
 }
